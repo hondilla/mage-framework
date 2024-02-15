2b75a51fd9c7f4e304de6681f48b55934f6bc86d
hondilla <7813599+hondilla@users.noreply.github.com>
2024-02-15 16:54:08 +0100

    fix path composer patches

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types=1);
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__, 4))
     ->withRouting(function () {
